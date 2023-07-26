47d74182ccc8c14bbbfe0a4bcc8a76716132795c
Squire-A <139629361+Squire-A@users.noreply.github.com>
2023-07-26 10:59:12 +0100

    Day 24 - completed, added lines on each page at 10mm intervals

--- main.py
+++ main.py
@@ -14,10 +14,13 @@ for index, row in df.iterrows():
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
-    pdf.line(10, 21, 200, 21)
+
+    # Add lines
+    for y in range(21, 298, 10):
+        pdf.line(10, y, 200, y)
 
     # Set the footer
-    pdf.ln(265)
+    pdf.ln(266)
     pdf.set_font(family="Times", style="I", size=8)
     pdf.set_text_color(180, 180, 180)
     pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
@@ -26,8 +29,12 @@ for index, row in df.iterrows():
     for i in range(row["Pages"]-1):
         pdf.add_page()
 
+        # Add lines
+        for y in range(21, 298, 10):
+            pdf.line(10, y, 200, y)
+
         # Set the footer
-        pdf.ln(277)
+        pdf.ln(278)
         pdf.set_font(family="Times", style="I", size=8)
         pdf.set_text_color(180, 180, 180)
         pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
